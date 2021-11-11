6c81cb49633340cc63e8f9555c6095bb48a57251
Satyam Prajapati <satyam7355pra@gmail.com>
2021-11-11 22:30:22 +0530

    third commit by satyam

---
+++ app/src/main/java/com/satyam/androidtestjob1/tester.java
@@ -0,0 +1,14 @@
+package com.satyam.androidtestjob1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tester extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tester);
+    }
+}
